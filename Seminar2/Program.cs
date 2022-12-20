2cdd5194df954ebbcc8da85387941f746d064c68
Andrey <tentbodys@gmail.com>
2022-12-20 22:13:09 +0300

    Completed homework and refactoring done

--- Seminar2/Program.cs
+++ Seminar2/Program.cs
@@ -18,9 +18,11 @@ Console.WriteLine("и выводит вторую цифру данного чи
 
 Console.Write("Введите, пожалуйста, трех значное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
+int minValue = 100;
+int maxValue = 999;
 
 // Инициализируем цикл на введение корректного значения если оно было не корректным.
-while(num < 100 || num > 999)
+while(num < minValue || num > maxValue)
 {
     Console.WriteLine("Было введено не корректное число!");
     Console.Write("Введите, пожалуйста, трех значное число: ");
@@ -38,26 +40,26 @@ Console.WriteLine($"Вторая цифра от числа {num} равняет
 или сообщает, что третьей цифры нет.
 */
 
-
+/*
 
 int CountingNumbers(int number)
 {
     int copyNumber = number;
-    int amountOfNumber = 0;
+    int amountOfNumbers = 0;
 
     if(copyNumber == 0)
-        amountOfNumber++;
+        amountOfNumbers++;
     else
     {
         // Цикл в котором отсекаем последнюю цифру и увеличиваем amountOfNumber (количество цифр).
         while(copyNumber != 0)
         {
             copyNumber /= 10;
-            amountOfNumber++;
+            amountOfNumbers++;
         }
     }
 
-    return amountOfNumber;
+    return amountOfNumbers;
 }
 
 int ShowThirdNumber(int number, int amount)
@@ -81,6 +83,7 @@ Console.WriteLine("-------------------------------------------------------------
 Console.WriteLine("Данная программа принимает любое целове число и выводит третью цифру.");
 Console.WriteLine("Если 3-ей цифры нет, то программа сообщает об этом.");
 Console.WriteLine("---------------------------------------------------------------------\n");
+
 Console.Write("Введите, пожалуйста, любое целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int amountOfNum = CountingNumbers(num);
@@ -95,3 +98,32 @@ if(amountOfNum >= 3)
 }
 else
     Console.WriteLine("В заданном числе нет третьей цифры!\n");
+*/
+
+/*
+Задача 3
+Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+и проверяет, является ли этот день выходным.
+*/
+
+/*
+
+bool DetermineDayOff(int day)
+{
+    if(day == 6 || day == 7) return true;
+    else return false;
+}
+
+Console.WriteLine("---------------------------------------------------------------------");
+Console.WriteLine("Данная программа принимает на вход цифру и выводит сообщение");
+Console.WriteLine("является ли это выходным или нет.");
+Console.WriteLine("---------------------------------------------------------------------\n");
+
+Console.Write("Пожалуйста, введите число: ");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+
+if(DetermineDayOff(dayOfWeek))
+    Console.WriteLine("Данный день является выходным!");
+else
+    Console.WriteLine("Данный день не является выходным.");
+*/
